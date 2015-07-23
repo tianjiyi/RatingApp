6f6f4081d95d43082138d9d864ed3f3b13f6c4fd
Jiyi Tian <jtian@Xinwens-MacBook-Air.local>
2015-07-22 22:20:59 -0700

    Initial Commit

---
+++ TestStoryBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestStoryBoard
+//
+//  Created by Jiyi Tian on 7/22/15.
+//  Copyright (c) 2015 Jiyi Tian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
