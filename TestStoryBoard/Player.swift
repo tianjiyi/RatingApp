8797ba90f369069b138dc34ee10e232f699ecd03
Jiyi Tian <jtian@Xinwens-MacBook-Air.local>
2015-07-28 18:10:56 -0700

    first commit TestStoryBoard

---
+++ TestStoryBoard/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  TestStoryBoard
+//
+//  Created by Jiyi Tian on 7/22/15.
+//  Copyright (c) 2015 Jiyi Tian. All rights reserved.
+//
+
+import Foundation
---
+++ TestStoryBoard/PlayerCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerCell.swift
+//  TestStoryBoard
+//
+//  Created by Jiyi Tian on 7/24/15.
+//  Copyright (c) 2015 Jiyi Tian. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
